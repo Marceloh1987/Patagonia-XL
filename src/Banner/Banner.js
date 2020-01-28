b59a40b4d26d487d9a1856b60f01311119e5427a
jamoragar <javier.moragarojas@gmail.com>
2020-01-27 23:48:04 -0300

    UPDATE - Se cambia Banner y la imagen del banner se mueve a el directorio del componente

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,8 +1,19 @@
-import styled from 'styled-components';
+import React from 'react'
+import './Banner.css';
 
-export const Banner = styled.div`
-    height: 500px;
-    background-image: url("img/Banner.jpg"); 
-    background-position: center;
-    background-size: cover;
-`
+export const Banner = () => {
+    return(
+        <div className="jumbotron jumbotron-fluid bg-dark">
+            <div className="jumbotron-background">
+                <img src={require('./Banner.jpg')} className="blur " alt="Servicios Escort"/>
+            </div>
+
+            <div className="container text-white">
+                <p className="lead">Sandwich, Pichangas, Completos y más</p>
+                <hr className="my-4" />
+                <p>Ahora con sistema de Pago en Línea <br/>Siguenos en Facebook!</p>
+                <div className="btn fb-color" role="button">Patagonia XL</div>
+            </div>
+        </div>
+    )
+}

eae5653b339c769241c74ebb07d3a5d071ff95e5
Marceloh1987 <55755584+Marceloh1987@users.noreply.github.com>
2020-02-27 18:06:17 -0300

    Revert "Revert "FIX - MERGE""
    This reverts commit c9dd4af74b71710221f7060423545cfdb8f96690.

--- src/Pago/Espera.js
+++ src/Pago/Espera.js
@@ -4,11 +4,39 @@ import { Title } from '../Styles/Title';
 import { NavBar } from '../Navbar/Navbar';
 import { Banner } from '../Banner/Banner';
 import { Footer } from '../Footer/Footer';
+import { Spinner, Jumbotron, Card } from 'react-bootstrap';
 
-const Espera = styled(Title)`
-    font-size: 30px;
+const JumboContainer = styled(Title)`
     
-    
+    .jumbotron {
+        padding-top: 0;
+    }
+
+    .card {
+        margin-top: -40px;
+        margin-bottom: -40px;
+        margin-left: 30%;
+        margin-right: 30%;
+        border: none;
+    }
+
+    .card-header {
+        color: #ffffff;
+        font-size: 20px;
+        background-color: #007bff;
+        margin-top: 0%;
+    }
+
+    .card-body {
+        color: #000000;
+        font-size: 30px;
+    }
+
+    .card-footer {
+        color: #ffffff;
+        background-color: #007bff;
+    }    
+
 `;
 
 export function EsperaDialog(){
@@ -16,7 +44,22 @@ export function EsperaDialog(){
         <>
         <NavBar/>
         <Banner/>
-        <Espera>Tu pago esta siendo procesado...</Espera>
+        <JumboContainer>
+            <Jumbotron fluid>
+                <Card className="text-center">
+                    <Card.Header>Estado Del Pago:</Card.Header>
+                    <Card.Body>
+                        <Card.Title></Card.Title>
+                        <Card.Text>
+                            En Proceso
+                            <br/>  
+                            <Spinner animation="grow" size="sm" />
+                            <Spinner animation="grow" size="md"/>
+                        </Card.Text>                                        
+                    </Card.Body>
+                </Card>   
+            </Jumbotron>
+        </JumboContainer>
         <Footer/>
         </>
 

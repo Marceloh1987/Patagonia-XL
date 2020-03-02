73574ff78cb8ac65b530eb882c0d56b5f6d3b983
Marcelo Hernández <marceloh1987@gmail.com>
2020-03-02 14:13:21 -0300

    Comandas 02-03-2020

--- src/Dashboard/Comandas/Comandas.js
+++ src/Dashboard/Comandas/Comandas.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
+import {Card, Button} from 'react-bootstrap';
 import ComandaStyles from "../MainContainer.module.scss";
 import { Order } from '../../Order/Order';
 
@@ -14,19 +14,81 @@ const Comandas = () => {
                         <Card bg="primary" text="white" style={{ width: '18rem' }}>
                             <Card.Header>Detalle de comanda</Card.Header>                          
                             <Card.Body>
-                                <Card.Title>Pedido: </Card.Title>
+                                <Card.Title>Pedido: N° de comanda  </Card.Title>
                                 <Card.Text>
                                    <p>Nombre: </p>
                                    <p>Apellido: </p>
                                    <p>Direccion: </p>
                                    <p>Telefono: </p>
                                    <p>Tipo de pago: </p>
-                                   <p></p>
+                                   <p>pedido: </p>
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer>
-                                <button>En proceso</button>
-                                <button>Listo!</button>
+                                <div>
+                                <Button variant="outline-light">En proceso</Button>{' '}
+                                <Button variant="outline-light">Listo!</Button>{' '}
+                                </div>
+                            </Card.Footer>
+                        </Card>
+                        <Card bg="warning" text="white" style={{ width: '18rem' }}>
+                            <Card.Header>Detalle de comanda</Card.Header>                          
+                            <Card.Body>
+                                <Card.Title>Pedido: N° de comanda  </Card.Title>
+                                <Card.Text>
+                                   <p>Nombre: </p>
+                                   <p>Apellido: </p>
+                                   <p>Direccion: </p>
+                                   <p>Telefono: </p>
+                                   <p>Tipo de pago: </p>
+                                   <p>pedido: </p>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <div>
+                                <Button variant="outline-light">En proceso</Button>{' '}
+                                <Button variant="outline-light">Listo!</Button>{' '}
+                                </div>
+                            </Card.Footer>
+                        </Card>
+                        <Card bg="danger" text="white" style={{ width: '18rem' }}>
+                            <Card.Header>Detalle de comanda</Card.Header>                          
+                            <Card.Body>
+                                <Card.Title>Pedido: N° de comanda  </Card.Title>
+                                <Card.Text>
+                                   <p>Nombre: </p>
+                                   <p>Apellido: </p>
+                                   <p>Direccion: </p>
+                                   <p>Telefono: </p>
+                                   <p>Tipo de pago: </p>
+                                   <p>pedido: </p>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <div>
+                                <Button variant="outline-light">En proceso</Button>{' '}
+                                <Button variant="outline-light">Listo!</Button>{' '}
+                                </div>
+                            </Card.Footer>
+                        </Card>
+                        <Card bg="success" text="white" style={{ width: '18rem' }}>
+                            <Card.Header>Detalle de comanda</Card.Header>                          
+                            <Card.Body>
+                                <Card.Title>Pedido: N° de comanda  </Card.Title>
+                                <Card.Text>
+                                   <p>Nombre: </p>
+                                   <p>Apellido: </p>
+                                   <p>Direccion: </p>
+                                   <p>Telefono: </p>
+                                   <p>Tipo de pago: </p>
+                                   <p>pedido: </p>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <div>
+                                <Button variant="outline-light">En proceso</Button>{' '}
+                                <Button variant="outline-light">Listo!</Button>{' '}
+                                </div>
                             </Card.Footer>
                         </Card>
                     </div>
